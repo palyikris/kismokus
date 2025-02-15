a290a591ba62ccae99a48fa65bb7f36198209495
Kribbany <palyi.kristof@gmail.com>
2025-02-15 22:21:31 +0100

    New firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEMwh0aA8c5FfHHMMdSP5Smg_j0nMF7lg",
-  authDomain: "dreamapartmanok.firebaseapp.com",
-  projectId: "dreamapartmanok",
-  storageBucket: "dreamapartmanok.appspot.com",
-  messagingSenderId: "802357793376",
-  appId: "1:802357793376:web:4ad88a404366210ca8a905",
-  measurementId: "G-NPW1SX5P6V"
+  apiKey: "AIzaSyBIuzS79tYWufa9miBLj7jvYzYoj1_E5GM",
+  authDomain: "nagymaros-69279.firebaseapp.com",
+  projectId: "nagymaros-69279",
+  storageBucket: "nagymaros-69279.firebasestorage.app",
+  messagingSenderId: "772766364226",
+  appId: "1:772766364226:web:74ea8301bb1608d1eff3db",
+  measurementId: "G-VJ4F4D7J1C"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
